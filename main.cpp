05cd2906eb6ef66ede311e37c6a713e757ef1f9b
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-26 11:33:15 +0530

    give an algorithm to find all pairs from set whose sum is S

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include<string>
 #include<map>
+#include<vector>
 #include<algorithm>
-#define pp pair<char,int>
+#define pp pair<int,int>
 using namespace std;
 void swapAndReduceSize(int * &arr,int i,int j)
 {
@@ -10,24 +11,31 @@ void swapAndReduceSize(int * &arr,int i,int j)
     arr[i]=arr[j];
     arr[j]=temp;
 }
-/***********give an algorithm to find first repeated character in a string brute force*/////////////
+/***********give an algorithm to find all pairs from set whose sum is S*///
 int main()
 {
-  string s;
-  cin>>s;
-  int n=s.size();
-  int i=0,j;
-  for(i=0;i<n-1;i++)
-  {
-      j=i+1;
-      while(j<n&&s[i]!=s[j])
-      {
-          j++;
-      }
-      if(j!=n)
-      {
-          cout<<s[i];
-          break;
-      }
-  }
+  vector<pair<int,int>> vector_;
+  map<int,int> map_;
+  int *arr,n,k,i;
+  cin>>n>>k;
+  for(i=0;i<n;i++)
+   {
+        cin>>arr[i];
+        map_[arr[i]]++;
+   }
+   for(int i=0;i<n;i++)
+   {
+       if(map_[arr[i]]!=0)
+       {
+
+           vector_.push_back(make_pair(arr[i],k-arr[i]));
+           map_[arr[i]]--;
+       }
+   }
+   for(auto it=vector_.begin();it!=vector_.end();it++)
+   {
+       cout<<it->first<<" "<<it->second<<endl;
+   }
+
+
 }
