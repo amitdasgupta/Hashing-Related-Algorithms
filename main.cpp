ddfbe9ecb27ac2fb0193f7999785426c1043bce8
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-01-26 09:58:01 +0530

    give an algorithm to find first non repeated character in a string

--- main.cpp
+++ main.cpp
@@ -10,33 +10,28 @@ void swapAndReduceSize(int * &arr,int i,int j)
     arr[i]=arr[j];
     arr[j]=temp;
 }
-/***********give an algorithm to remove specified characters from the given string*/////////////
+/***********give an algorithm to find first non repeated character in a string*/////////////
 int main()
 {
-  int arr[256],i;
-  for(i=0;i<257;i++)
-    arr[i]=0;
-  string s,p;
-  cin>>s>>p;
-  int n=p.size();
+  string s;
+  map<char,int> map_;
+  cin>>s;
+  int n=s.size();
   for(int i=0;i<n;i++)
   {
-      arr[p[i]]++;
-  }
-  n=s.size();
-  i=0;
-  while(i<n)
-  {
-      if(arr[s[i]]!=0)
+      if(map_[s[i]]!=0)
       {
-          /*** add this line if multiple characters are repeated in pattern
-          arr[s[i]]--;*/////
-          s[i]=s[n-1];
-          n--;
+          map_[s[i]]++;
       }
       else
-        i++;
+        map_[s[i]]=1;
   }
-  s.resize(n);
-  cout<<s;
+  for(int i=0;i<n;i++)
+  {
+      if(map_[s[i]]==1)
+      {
+          cout<<s[i];
+          break;
+      }
+  }
 }
